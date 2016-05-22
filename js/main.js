15480d70f446724e7c062e90a2d6ce06f472f00f
Johnson Olayiwola <johnson.olayiwola@gmail.com>
2016-05-22 01:00:44 +0100

    JS added to all pages

--- js/main.js
+++ js/main.js
@@ -33,8 +33,43 @@ $(document).ready(function () {
 
   });
 
+// Clear Ideas Event
 	$('#clear-ideas').on('click', function(){
     clearAllIdeas();
+  });
+
+  // Add To-do list Event//
+  $('#add-task-form').on('submit', function(e){
+    addTask(e);
+  });
+
+  // Remove To-do List Event
+  $('#task-table').on('click', '#remove-task', function(){
+    id = $(this).data('id');
+    removeTask(id);
+
+  });
+
+  // Clear To-do List Event
+  $('#clear-tasks').on('click', function(){
+    clearAllTasks();
+  });
+
+  // Add Shopping list Items Event//
+  $('#add-shopping-form').on('submit', function(e){
+    addItems(e);
+  });
+
+  // Remove Shopping List Items Event
+  $('#shopping-table').on('click', '#remove-shopping', function(){
+    id = $(this).data('id');
+    removeItems(id);
+
+  });
+
+  // Clear Shopping List Items Event
+  $('#clear-shopping').on('click', function(){
+    clearAllItems();
   });
 
   displayExpenses();
@@ -158,7 +193,7 @@ $(document).ready(function () {
         'expense': expense,
         'amount': amount,
         'date': date
-      }
+      };
 
       expenses.push(new_expense);
       localStorage.setItem('expenses', JSON.stringify(expenses));
@@ -167,12 +202,12 @@ $(document).ready(function () {
 
   // Function to remove expense
   function removeExpense(id){
-    if(confirm('Are you sure you want to delete this task?')){
+    if(confirm('Are you sure you want to delete this expense?')){
       var expenseList = JSON.parse(localStorage.getItem('expenses'));
 
       for(var i=0; i < expenseList.length; i++){
         if(expenseList[i].id == id){
-          expenseList.splice(i,1)
+          expenseList.splice(i,1);
         }
         localStorage.setItem('expenses', JSON.stringify(expenseList));
       }
@@ -244,12 +279,12 @@ $(document).ready(function () {
 
   // Function to remove ideas
   function removeIdea(id){
-		if(confirm('Are you sure you want to delete this task?')){
+		if(confirm('Are you sure you want to delete this idea?')){
 			var ideaList = JSON.parse(localStorage.getItem('ideas'));
 
       for(var i=0; i < ideaList.length; i++){
         if(ideaList[i].id == id){
-          ideaList.splice(i,1)
+          ideaList.splice(i,1);
         }
         localStorage.setItem('ideas', JSON.stringify(ideaList));
       }
@@ -264,7 +299,176 @@ $(document).ready(function () {
     }
   }
 
+  /* ==========================================================================
+     Function for To-do List
+     ========================================================================== */
 
+  displayTasks();
+
+  function displayTasks(){
+    var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+    //Set Counter
+    var i = 0;
+    if(localStorage.getItem('tasks') !== null){
+      $.each(taskList, function(key,value){
+        $('#task-table').append('<tr id="'+ value.id + '">'+
+                       '<td>' + ++i + '</td>'+
+                       '<td>' + value.task + '</td>'+
+                       '<td>' + value.priority + '</td>'+
+                       '<td><a href="#" id="remove-task" data-id="'+ value.id+'">Remove</a></td>' + '</tr>');
+      });
+    }
+  }
+
+  // Function to add new tasks
+  function addTask(e){
+    var newDate = new Date();
+    var id = newDate.getTime();
+
+    var task = $('#task-list').val();
+    var priority = $('#priority').val();
+
+    /* Simple Validation*/
+    if(task == ''){
+      alert('Task is required');
+      e.preventDefault();
+    }else if(priority == '') {
+      alert('Priority is required');
+      e.preventDefault();
+    }else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+
+      //Check Tasks
+      if(tasks == null){
+        tasks = [];
+      }
+
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+      // New Task Object
+      var new_task = {
+        'id': id,
+        'task': task,
+        'priority': priority
+      };
+
+      tasks.push(new_task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+
+      console.log('Tasks Added');
+    }
+
+  }
+
+
+  // Function to remove tasks
+  function removeTask(id){
+    if(confirm('Are you sure you want to delete this task?')){
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+      for(var i=0; i < taskList.length; i++){
+        if(taskList[i].id == id){
+          taskList.splice(i,1);
+        }
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+      }
+      location.reload();
+    }
+  }
+
+  // Function to clear all tasks
+  function clearAllTasks(){
+    if(confirm('Do you want to clear all tasks?')){
+      localStorage.removeItem('tasks');
+      location.reload();
+    }
+  }
+
+  /* ==========================================================================
+     Function for Shopping List Items
+     ========================================================================== */
+
+  displayItems();
+
+  function displayItems(){
+    var shoppingList = JSON.parse(localStorage.getItem('shoppingItems'));
+
+    //Set Counter
+    var i = 0;
+    if(localStorage.getItem('shoppingItems') !== null){
+      $.each(shoppingList, function(key,value){
+        $('#shopping-table').append('<tr id="'+ value.id + '">'+
+                       '<td>' + ++i + '</td>'+
+                       '<td>' + value.shopping + '</td>'+
+                       '<td>' + value.cost + '</td>'+
+                       '<td><a href="#" id="remove-task" data-id="'+ value.id+'">Remove</a></td>' + '</tr>');
+      });
+    }
+  }
+
+  // Function to add new shopping item
+  function addItems(e){
+    var newDate = new Date();
+    var id = newDate.getTime();
+
+    var shopping = $('#shopping').val();
+    var cost = $('#cost').val();
+
+    /* Simple Validation*/
+    if(shopping == ''){
+      alert('Shopping item is required');
+      e.preventDefault();
+    }else if(cost == '') {
+      alert('Item Cost is required');
+      e.preventDefault();
+    }else {
+      shoppingItems = JSON.parse(localStorage.getItem('shoppingItems'));
+
+      //Check Shopping Items
+      if(shoppingItems == null){
+        shoppingItems = [];
+      }
+
+      var shoppingList = JSON.parse(localStorage.getItem('shoppingItems'));
+
+      // New Shopping Object
+      var new_shopping = {
+        'id': id,
+        'shopping': shopping,
+        'cost': cost
+      };
+
+      shoppingItems.push(new_shopping);
+      localStorage.setItem('shoppingItems', JSON.stringify(shoppingItems));
+
+      console.log('Shopping Items Added');
+    }
+
+  }
+
+
+  // Function to remove items
+  function removeItems(id){
+    if(confirm('Are you sure you want to delete this item?')){
+      var shoppingList = JSON.parse(localStorage.getItem('shoppingItems'));
+
+      for(var i=0; i < shoppingList.length; i++){
+        if(shoppingList[i].id == id){
+          shoppingList.splice(i,1);
+        }
+        localStorage.setItem('shoppingItems', JSON.stringify(shoppingList));
+      }
+      location.reload();
+    }
+  }
+
+  // Function to clear all to-buy items
+  function clearAllItems(){
+    if(confirm('Do you want to clear all shopping items?')){
+      localStorage.removeItem('shoppingItems');
+      location.reload();
+    }
+  }
 
 });
 
